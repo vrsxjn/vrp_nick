b7a48e8653d45eb3638868b5d0278ab60cc23256
Gabriel <47954728+V1ru6s@users.noreply.github.com>
2019-02-25 22:00:16 -0300

    Add files via upload

---
+++ vrp_nomes/cfg/config.lua
@@ -0,0 +1,18 @@
+config = {
+    range = 5,
+    self = false,
+    speaker = true,
+    name = true,
+    id = false,
+    admin_only = false,
+    lang = {
+        default = "en",
+        ['en'] = {
+            speaking = "Falando.."
+        },
+    },
+    colors = {
+        default = {r = 255, g = 255, b = 255, a = 225},
+        speaker = {r = 0, g = 150, b = 230, a = 225}
+    }
+}
